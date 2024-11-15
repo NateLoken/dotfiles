cd03c30be5d624db07583a41f50c9333164c5617
Nate Loken <nfloken@gmail.com>
2024-11-15 16:12:44 -0700

    Updated neovim package manager to use lazy instead of packer

---
+++ .config/nvim/lua/nloken/lazy/fugitive.lua
@@ -0,0 +1,32 @@
+return {
+    'tpope/vim-fugitive' ,
+
+    config = function()
+        local Nloken_Fugitive = vim.api.nvim_create_augroup("Nloken_Fugitive", {})
+        local autocmd = vim.api.nvim_create_autocmd
+
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        group = Nloken_Fugitive
+        pattern = "*"
+
+        callback = function()
+            if vim.bo.ft ~= "fugitive" then
+                return
+            end
+
+            local bufnr = vim.api.nvim_get_currentbf()
+            local opts = {buffer = bufnr, remap = false}
+
+            vim.keymap.set("n", "<leader>P", function()
+                vim.cmd.Git({'pull', '--rebase'})
+            end, opts)
+
+            -- Rebase always
+            vim.keymap.set("n", "<leader>P", function()
+                vim.cmd.Git({'pull', '--rebase'})
+            end, opts)
+
+            vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts)
+        end
+    end,
+}
--- .config/nvim/lua/nloken/packer.lua
+++ .config/nvim/lua/nloken/packer.lua
@@ -1,7 +1,8 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+--[[
+vim.cmd [[packadd packer.nvim
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
@@ -38,3 +39,4 @@ return require('packer').startup(function(use)
   }
 }
 end)
+--]]
