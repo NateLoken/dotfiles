5a994309c742bf3a53036403f98f14237e92a680
Nate Loken <nfloken@gmail.com>
2026-04-28 22:27:43 -0700

    Removed duplicate code in fugitive

--- nvim/lua/nloken/lazy/fugitive.lua
+++ nvim/lua/nloken/lazy/fugitive.lua
@@ -1,32 +1,28 @@
 return {
-    'tpope/vim-fugitive' ,
+	"tpope/vim-fugitive",
 
-    config = function()
-        local Nloken_Fugitive = vim.api.nvim_create_augroup("Nloken_Fugitive", {})
-        local autocmd = vim.api.nvim_create_autocmd
+	config = function()
+		local Nloken_Fugitive = vim.api.nvim_create_augroup("Nloken_Fugitive", {})
+		local autocmd = vim.api.nvim_create_autocmd
 
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        group = Nloken_Fugitive
-        pattern = "*"
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		group = Nloken_Fugitive
+		pattern = "*"
 
-        callback = function()
-            if vim.bo.ft ~= "fugitive" then
-                return
-            end
+		callback = function()
+			if vim.bo.ft ~= "fugitive" then
+				return
+			end
 
-            local bufnr = vim.api.nvim_get_currentbf()
-            local opts = {buffer = bufnr, remap = false}
+			local bufnr = vim.api.nvim_get_currentbf()
+			local opts = { buffer = bufnr, remap = false }
 
-            vim.keymap.set("n", "<leader>P", function()
-                vim.cmd.Git({'pull', '--rebase'})
-            end, opts)
+			-- Rebase always
+			vim.keymap.set("n", "<leader>P", function()
+				vim.cmd.Git({ "pull", "--rebase" })
+			end, opts)
 
-            -- Rebase always
-            vim.keymap.set("n", "<leader>P", function()
-                vim.cmd.Git({'pull', '--rebase'})
-            end, opts)
-
-            vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts)
-        end
-    end,
+			vim.keymap.set("n", "<leader>t", ":Git push -u origin", opts)
+		end
+	end,
 }
